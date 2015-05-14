b20f1d915aae7740819b34051320e4c206822d6e
masashi arino <masashi.arino@ultinet.co.jp>
2015-05-14 17:23:19 +0900

    テストクラス作成

---
+++ src/String/StringPracticeTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: web_admin
+ * Date: 15/05/14
+ * Time: 16:53
+ */
+
+namespace Test;
+
+use PHPUnit_Framework_TestCase;
+use String\StringPractice;
+
+class StringPracticeTest extends PHPUnit_Framework_TestCase
+{
+
+    /** @var StringPractice $stringPractice */
+    private $stringPractice;
+
+    public function setUp()
+    {
+        $this->stringPractice = new StringPractice();
+    }
+
+    /**
+     * @test
+     */
+    public function インスタンス生成直後は空()
+    {
+        self::assertEquals('', $this->stringPractice->getString());
+    }
+
+
+    /**
+     * @test
+     */
+    public function addStringメソッドを使うと文字が足せる()
+    {
+        $this->stringPractice->addString('hoge');
+        self::assertEquals('hoge', $this->stringPractice->getString());
+    }
+
+    /**
+     * @test
+     */
+    public function addStringメソッドを２回呼ぶと文字が倍になる()
+    {
+        $this->stringPractice->addString('hoge');
+        $this->stringPractice->addString('hoge');
+        self::assertEquals('hogehoge', $this->stringPractice->getString());
+
+    }
+
+}
